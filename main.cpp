085127c283597558aa86ce47d2aee9ecaef4bb2a
Bryan Sammon <bryansammon@yahoo.com>
2022-09-14 13:18:09 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <emscripten/bind.h>
+#include <emscripten/val.h>
+#include <stdexcept>
+#include "node_modules/easyexif/exif.h"
+
+using namespace emscripten;
+
+val exif(std::string img_in, size_t length)
+{
+  uint8_t *buffer = (uint8_t *)img_in.c_str();
+
+  easyexif::EXIFInfo result;
+  val info = val::object();
+
+  int status = result.parseFrom(buffer, length);
+
+  switch (status)
+  {
+  case 1982:
+    throw std::runtime_error("Not a valid JPEG!");
+  case 1983:
+    throw std::runtime_error("No EXIF found in JPEG!");
+  case 1984:
+    throw std::runtime_error("Invalid EXIF format!");
+  case 1985:
+    throw std::runtime_error("EXIF format corrupted!");
+  default:
+    info.set("make", result.Make.c_str());
+    info.set("model", result.Model.c_str());
+    info.set("software", result.Software.c_str());
+    info.set("bitsPerSample", result.BitsPerSample);
+    info.set("imageWidth", result.ImageWidth);
+    info.set("imageHeight", result.ImageHeight);
+    info.set("imageDescription", result.ImageDescription.c_str());
+    info.set("orientation", result.Orientation);
+    info.set("copyright", result.Copyright.c_str());
+    info.set("dateTime", result.DateTime.c_str());
+    info.set("dateTimeOriginal", result.DateTimeOriginal.c_str());
+    info.set("dateTimeDigitized", result.DateTimeDigitized.c_str());
+    info.set("subSecTimeOriginal", result.SubSecTimeOriginal.c_str());
+    info.set("exposureTime", result.ExposureTime);
+    info.set("fStop", result.FNumber);
+    info.set("exposureProgram", result.ExposureProgram);
+    info.set("isoSpeedRatings", result.ISOSpeedRatings);
+    info.set("exposureProgram", result.ExposureProgram);
+    info.set("subjectDistance", result.SubjectDistance);
+    info.set("exposureBiasValue", result.ExposureBiasValue);
+    info.set("flash", result.Flash);
+    info.set("flashReturnedLight", result.FlashReturnedLight);
+    info.set("flashMode", result.FlashMode);
+    info.set("meteringMode", result.MeteringMode);
+    info.set("focalLength", result.FocalLength);
+    info.set("focalLengthIn35mm", result.FocalLengthIn35mm);
+
+    val geo = val::object();
+    geo.set("latitude", result.GeoLocation.Latitude);
+    geo.set("latDegrees", result.GeoLocation.LatComponents.degrees);
+    geo.set("latMinutes", result.GeoLocation.LatComponents.minutes);
+    geo.set("latSeconds", result.GeoLocation.LatComponents.seconds);
+    geo.set("latDirection", result.GeoLocation.LatComponents.direction);
+
+    geo.set("longitude", result.GeoLocation.Longitude);
+    geo.set("lonDegrees", result.GeoLocation.LonComponents.degrees);
+    geo.set("lonMinutes", result.GeoLocation.LonComponents.minutes);
+    geo.set("lonSeconds", result.GeoLocation.LonComponents.seconds);
+    geo.set("lonDirection", result.GeoLocation.LonComponents.direction);
+
+    geo.set("altitude", result.GeoLocation.Altitude);
+    geo.set("precisionDOP", result.GeoLocation.DOP);
+    info.set("geoLocation", geo);
+
+    val lens_info = val::object();
+    lens_info.set("focalLengthMin", result.LensInfo.FocalLengthMin);
+    lens_info.set("focalLengthMax", result.LensInfo.FocalLengthMax);
+    lens_info.set("fStopMin", result.LensInfo.FStopMin);
+    lens_info.set("fStopMax", result.LensInfo.FStopMax);
+    lens_info.set("lensMake", result.LensInfo.Make.c_str());
+    lens_info.set("lensModel", result.LensInfo.Model.c_str());
+    lens_info.set("focalPlaneXResolution", result.LensInfo.FocalPlaneXResolution);
+    lens_info.set("focalPlaneYResolution", result.LensInfo.FocalPlaneYResolution);
+    info.set("lensInfo", lens_info);
+  }
+
+  return info;
+}
+
+EMSCRIPTEN_BINDINGS(Exif)
+{
+  function("exif", &exif);
+}
---
+++ tests/wasm_exif.test.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import fetch from 'node-fetch';
+import wasm_exif, { ExifModule } from '../wasm_exif';
+
+const RANDOM_URL = 'https://source.unsplash.com/random/';
+
+describe('Exif', () => {
+  let exifModule: ExifModule;
+
+  beforeAll(async () => {
+    exifModule = (await wasm_exif({
+      noInitialRun: true,
+    })) as ExifModule;
+  });
+
+  it('reads exif information from a .jpeg', async () => {
+    const [inWidth, inHeight] = [1280, 720];
+    const buffer = new Uint8Array(
+      await fetch(`${RANDOM_URL}${inWidth}x${inHeight}`, {}).then(res =>
+        res.buffer()
+      )
+    );
+    const { exif } = exifModule;
+
+    // Unsplash images don't have EXIF information included, so function should throw
+    expect(() => exif(buffer, buffer.length)).toThrow();
+  });
+});
